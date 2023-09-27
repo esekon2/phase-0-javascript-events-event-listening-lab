34aa994748cbf15131e6a479af91396ea62ac63c
Densel <esekondensel@gmail.com>
2023-09-27 22:46:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const input = document.getElementById('input');
+  
+    input.addEventListener('click', function() {
+      alert('I Love you!');
+    });
+  }
+  
